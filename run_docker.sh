1d5acff9fad252f53271a18f7de726505c9abca3
George <george.nizharadze@crowleycarbon.com>
2020-01-04 18:16:56 +0200

    fix: Replace config.yml and fix run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker image build --tag 195011/price_predictor:1.0 .
+docker image build --tag 195011/price-predictor:1.0 .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker container run --rm -p 8000:80 --name price_predictor 195011/price_predictor:1.0
+docker container run --rm -p 8000:80 --name price_predictor 195011/price-predictor:1.0
